a466d7de868457d3514b6e3f33e24f81568ffd1b
Vijet Honnavar <vijet.honnavar@QUALITEST.LOCAL>
2021-12-28 11:30:18 -0500

    Added git actions file

--- java_tests/src/test/java/tests/BaseTest.java
+++ java_tests/src/test/java/tests/BaseTest.java
@@ -59,6 +59,23 @@ public class BaseTest {
                     ChromeOptions browserOptions = new ChromeOptions();
                     driver = new ChromeDriver();
                 }
+            } else if (host.equals("saucelabs-connect")) {
+                MutableCapabilities sauceOptions = new MutableCapabilities();
+                sauceOptions.setCapability("username", sauceUser);
+                sauceOptions.setCapability("accessKey", sauceKey);
+                sauceOptions.setCapability("name", testName);
+                sauceOptions.setCapability("tunnelIdentifier", sauceTunnel );
+                sauceOptions.setCapability("parentTunnel", sauceParentTunnel);
+                MutableCapabilities capabilities = new MutableCapabilities();
+                capabilities.setCapability("browserName", browserName);
+                capabilities.setCapability("browserVersion", browserVersion);
+                capabilities.setCapability("platformName", platformName);
+                capabilities.setCapability("sauce:options", sauceOptions);
+                String sauceUrl = String.format("https://ondemand.saucelabs.com/wd/hub");
+                driver = new RemoteWebDriver(new URL(sauceUrl), capabilities);
+                sessionId = ((RemoteWebDriver) driver).getSessionId().toString();
+                sauceClient = new SauceREST(sauceUser, sauceKey, DataCenter.US);
+
             }
         }
 
--- java_tests/src/test/java/tests/Config.java
+++ java_tests/src/test/java/tests/Config.java
@@ -3,12 +3,13 @@ package tests;
 
 public class Config {
     public static final String baseUrl = System.getProperty("baseUrl", "http://the-internet.herokuapp.com");
-    public static final String host = System.getProperty("host", "saucelabs");
+    public static final String host = System.getProperty("host", "saucelabs-connect");
     public static final String browserName = System.getProperty("browserName", "chrome");
     public static final String browserVersion = System.getProperty("browserVersion", "75.0");
     public static final String platformName = System.getProperty("platformName", "Windows 10");
     public static final String sauceUser = System.getenv("SAUCE_USERNAME");
     public static final String sauceKey = System.getenv("SAUCE_ACCESS_KEY");
     public static final String sauceTunnel = System.getenv("SAUCE_TUNNEL_NAME");
+    public static final String sauceParentTunnel = System.getenv("SAUCE_PARENT_TUNNEL");
 
 }
